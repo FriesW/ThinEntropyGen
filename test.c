16f775690140ac655cddcf5a819bac3bffbfd3a7
William Fries <wjf4578@truman.edu>
2018-06-06 17:20:12 -0500

    Better hex printing.

--- test.c
+++ test.c
@@ -7,8 +7,13 @@ int main(int ac, char** argv){
     char hash[SHA512_DIGEST_LENGTH];
     SHA512(data, sizeof(data) - 1, hash);
 
-    for(int i = 0; i < SHA512_DIGEST_LENGTH; i++)
-        printf("%hhX", hash[i]);
+    for(int i = 0; i < SHA512_DIGEST_LENGTH; i++) {
+        if(i == 0);
+        else if(i % SHA512_DIGEST_LENGTH == 0) printf("\n\n"); //TODO check
+        else if(i % 16 == 0) printf("\n");
+        else if(i % 2 == 0) printf(" ");
+        printf("%02X", hash[i] & 0xFF);
+    }
     //printf("%s", hash);
     printf("\n");
     return 0;
