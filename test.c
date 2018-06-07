0fd2d8146ba0c96dba11641d3f380f533a8cebaa
William Fries <wjf4578@truman.edu>
2018-06-06 20:14:51 -0500

    Hash tree iterator done.

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 
 typedef unsigned int uint;
 
-#define TREE_DEPTH 3 
+#define TREE_DEPTH 4 
 #define DIGEST_LEN SHA512_DIGEST_LENGTH
 #define DIGEST SHA512
 
@@ -32,19 +32,28 @@ int main(int ac, char** argv){
 
     const uint BUFF_SIZE = hash_count_at(TREE_DEPTH) * DIGEST_LEN;
 
-    //printf("Digest size: %i\n", SHA512_DIGEST_LENGTH);
-    //printf("Buffer size: %i\n", BUFF_SIZE);
-
     char data[] = "data to hash";
-    char hash[BUFF_SIZE];
-    DIGEST(data, sizeof(data) - 1, hash);
+    char buffer[BUFF_SIZE];
+
+    for(uint level = 1; level <= TREE_DEPTH; level++) {
+        for(uint hash_index = hash_count_at(level) - 1; //Count to index
+         hash_index != -1; hash_index--) {
+            printf("Level: %i", level);
+            printf("\tHash ind: %i", hash_index);
+            printf("\tHash pos: %i", offset_hash_at(hash_index));
+            printf("\tHash src pos: %i", offset_source_at(hash_index));
+            printf("\n");
+        }
+    }
+
+    DIGEST(data, sizeof(data) - 1, buffer);
 
     for(uint i = 0; i < BUFF_SIZE; i++) {
         if(i == 0);
         else if(i % DIGEST_LEN == 0) printf("\n\n"); //TODO check
         else if(i % 16 == 0) printf("\n");
         else if(i % 2 == 0) printf(" ");
-        printf("%02X", hash[i] & 0xFF);
+        printf("%02X", buffer[i] & 0xFF);
     }
     //printf("%s", hash);
     printf("\n");
